e229710d86d87e6fb49b4350e94306aad4e5b81e
Tetsumote <unionproject88@gmail.com>
2017-06-26 00:21:56 +0200

    creating partials

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,11 @@
-var myApp = angular.module('myApp', []);
+var artistControllers = angular.module('artistControllers', []);
 
-myApp.controller('MyController', function MyController($scope, $http){
+artistControllers.controller('ListController', ['$scope','$http', function($scope, $http){
     $http.get('js/data.json').then(function(response){
     $scope.artists = response.data;
-   
+    $scope.artistOrder = 'name';
     });
   
 
-});
+}]);
 
